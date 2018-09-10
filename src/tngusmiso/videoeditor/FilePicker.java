99832d95be48066d5c71931a66fabe6a88e540b7
tngusmiso <k1miso012@gmail.com>
2018-09-10 19:19:31 +0900

    create class FilePicker

---
+++ src/tngusmiso/videoeditor/FilePicker.java
@@ -0,0 +1,44 @@
+package tngusmiso.videoeditor;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.*;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+ 
+public class FilePicker { 
+	private JFileChooser fc;
+	private FileNameExtensionFilter audioFilter;
+	private FileNameExtensionFilter videoFilter;
+	private FileNameExtensionFilter imageFilter;	
+	private String filePath;
+	
+	public FilePicker(){
+		fc = new JFileChooser();
+		audioFilter = new FileNameExtensionFilter("audio", "mp3", "wav");
+		videoFilter = new FileNameExtensionFilter("video","mp4", "mpg", "mpeg", "avi", "mkv");
+		imageFilter = new FileNameExtensionFilter("images", "bmp", "png", "gif", "jpg", "jpeg");
+		filePath = null;
+	}
+	
+	public void ChooseFiles() {
+		fc.setFileFilter(audioFilter);
+		fc.addChoosableFileFilter(videoFilter);
+		fc.addChoosableFileFilter(imageFilter);
+		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		int ret = fc.showOpenDialog(null);
+		
+		if(ret != JFileChooser.APPROVE_OPTION) {
+			JOptionPane.showMessageDialog(null,"파일을 선택하지 않았습니다.","경고",JOptionPane.WARNING_MESSAGE);
+			return;
+		}
+		else {
+			filePath = fc.getSelectedFile().getPath();
+		}
+	}
+     
+	public String getFilePath() {
+		return filePath;
+	}
+
+}
