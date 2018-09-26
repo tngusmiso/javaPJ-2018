3a95ac713d2fe659eb7ccdf8fbf414397ec614e7
tngusmiso <k1miso012@gmail.com>
2018-09-26 20:33:03 +0900

    0926 Add playButton Click Listener

--- src/tngusmiso/videoeditor/PreviewPanel.java
+++ src/tngusmiso/videoeditor/PreviewPanel.java
@@ -2,6 +2,8 @@ package tngusmiso.videoeditor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,7 +19,7 @@ public class PreviewPanel extends JPanel{
 
 		videoPanel = new VideoPanel();
 		controllerPanel = new ControllerPanel();
-		
+
 		add(videoPanel, BorderLayout.CENTER);
 		add(controllerPanel, BorderLayout.SOUTH);
 		
@@ -30,23 +32,21 @@ public class PreviewPanel extends JPanel{
 		
 		private VideoPanel() {
 			try {
-				file = VideoEditor.resourceView.getFileArrayList().get(0);
 				createPlayer();
 			}
 			catch (Exception e) {
-				JLabel label = new JLabel("재생할 파일이 없습니다.");
-				add(label);
 			}
 		}
 		
 		private void createPlayer() {
+			file = VideoEditor.resourceView.getFileArrayList().get(0);
 			if ( file == null )
 		         return;
 			removePreviousPlayer();
 			
 			try {
 				// create a new player and add listener
-				player = Manager.createPlayer( file.toURL() );
+				player = Manager.createPlayer( file.toURI().toURL() );
 				controller = player.getControlPanelComponent();
 				player.start();  // start player
 			}
@@ -89,6 +89,12 @@ public class PreviewPanel extends JPanel{
 			add(playButton);
 			add(timeLabel);
 			add(timeSlider);
+			
+			playButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					videoPanel.createPlayer();
+				}
+			});
 		}
 	}
 }
